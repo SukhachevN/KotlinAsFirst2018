b322ba0ddd5fdac8cadd29e27f05de6866424ed5
SukhachevN <traed.traed@mail.ru>
2018-10-07 19:50:39 +0300

    задачи сделаны

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -56,21 +56,20 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int {
+fun daysInMonth(month: Int, year: Int): Int =
 
-    return when (month) {
-        1, 3, 5, 7, 8, 10, 12 -> 31
-        2 -> {
-            if ((year % 4 != 0 || year % 100 == 0) && year % 400 != 0) {
-                28
-            } else {
-                29
+        when (month) {
+            1, 3, 5, 7, 8, 10, 12 -> 31
+            2 -> {
+                if ((year % 4 != 0 || year % 100 == 0) && year % 400 != 0) {
+                    28
+                } else {
+                    29
+                }
             }
+            else -> 30
         }
-        else -> 30
-    }
 
-}
 
 /**
  * Средняя
@@ -94,12 +93,11 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
-    return when {
-        (a <= r) && (b <= s || c <= s) -> true
-        (a <= s) && (b <= r || c <= r) -> true
-        (b <= r) && (c <= s) -> true
-        (b <= s) && (c <= r) -> true
-        else -> false
-    }
-}
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean =
+
+        (a <= r) && (b <= s || c <= s) ||
+                (a <= s) && (b <= r || c <= r) ||
+                (b <= r) && (c <= s) ||
+                (b <= s) && (c <= r)
+
+
