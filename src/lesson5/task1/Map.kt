9d29b27a9e2ce6db377802296a3c32aab4af64dc
SukhachevN <traed.traed@mail.ru>
2018-11-02 19:48:53 +0300

    задачи сделаны

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -416,14 +416,28 @@ fun hasAnagrams(words: List<String>): Boolean {
 fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
     var i = number
     var k = 0
-    do {
-        if (k in list && i in list && list.indexOf(k) != list.indexOf(i)) {
-            return Pair(list.indexOf(k), list.indexOf(i))
-        } else {
-            i--
-            k++
+    if (i != 0) {
+        do {
+            if (k in list && i in list && list.indexOf(k) != list.indexOf(i)) {
+                return Pair(list.indexOf(k), list.indexOf(i))
+            } else {
+                i--
+                k++
+            }
+        } while (k <= number / 2)
+    } else {
+        val list1 = list.toMutableList()
+        if (0 in list1) {
+            val a = list1.indexOf(0)
+            list1.removeAt(a)
+            if (0 in list1) {
+                if (list1.indexOf(0) == a) {
+                    val b = list1.indexOf(0) + 1
+                    return Pair(a, b)
+                }
+            }
         }
-    } while (k <= number / 2)
+    }
     return Pair(-1, -1)
 }
 
