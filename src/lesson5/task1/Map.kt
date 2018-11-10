d745939c89b81a466e044b1d234f9e9366221f6b
SukhachevN <traed.traed@mail.ru>
2018-11-10 20:56:57 +0300

    задачи сделаны

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -247,6 +247,7 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
 fun propagateHandshakes(friends: Map<String, Set<String>>): Map<String, Set<String>> {
     val result = mutableMapOf<String, Set<String>>()
     val all = mutableSetOf<String>()
+    all + friends.keys
     for (value in friends.values) {
         all += value
     }
