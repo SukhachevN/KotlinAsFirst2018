e23565613d02af05be70fbc9d81ce10562e61800
SukhachevN <traed.traed@mail.ru>
2018-12-15 18:28:57 +0300

    задачи сделаны

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -371,9 +371,7 @@ fun chooseLongestChaoticWord(inputName: String, outputName: String) {
     for (string in text) {
         val flag = string.toLowerCase().toSet().size == string.length
         if (flag) {
-            if (string !in list) {
-                list.add(string)
-            }
+            list.add(string)
             if (string.length >= maxLength) {
                 maxLength = string.length
             }
